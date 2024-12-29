387d15ea742bb1efc65818534e42c843a59843e0
Matebook <adilbek200620@gmail.com>
2024-12-29 15:27:30 +0500

    init project

---
+++ models/Person.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    public String name;
+    public String surname;
+    public int age;
+    public boolean gender;
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String getGender = gender ? "male" : "female";
+        return "Hi, i am " + name + " " + surname + ", a " + age +" " + "year old" + " " + getGender;
+    }
+    }
+
+
---
+++ models/Teacher.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends Person {
+        String subject;
+        int yearsOfExperience;
+        int salary;
+        public Teacher(String name, String surname, int age, boolean gender , String subject, int yearsOfExperience, int salary) {
+            super(name, surname, age, gender);
+            this.subject = subject;
+            this.yearsOfExperience = yearsOfExperience;
+            this.salary = salary;
+        }
+
+        public double giveRaise(double percentage) {
+            if (percentage > 0 && yearsOfExperience > 10) {
+                salary += salary * percentage / 100;
+            }
+            return salary;
+        }
+        @Override
+        public String toString() {
+            return super.toString() + " I teach" + " " + subject;
+        }
+    }
+
